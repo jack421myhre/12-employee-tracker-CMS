0b035b6f0b1f250c3dd5b358c0f7a39fd915f2b2
Jackson Myhre <jack421myhre@gmail.com>
2022-10-04 15:44:43 -0500

    init

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Lead Engineer', 100000, 2);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ('John', 'Doe');
